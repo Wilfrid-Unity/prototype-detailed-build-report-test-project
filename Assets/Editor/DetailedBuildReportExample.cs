3fbd939b46106e8cb3ab3c23ab9010ad99a0ad50
Wilfrid Girard <wilfrid@unity3d.com>
2019-09-03 13:29:15 +0900

    Add Editor "Build" menu illustrating how to use prototype feature BuildOptions.DetailedBuildReport

---
+++ Assets/Editor/DetailedBuildReportExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DetailedBuildReportExample : MonoBehaviour
+{
+    [MenuItem("Build/DetailedBuildReport example")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[]
+        {
+            "Assets/Scenes/Cube.unity",
+            "Assets/Scenes/Planes.unity",
+            "Assets/Scenes/Sphere.unity"
+        };
+        buildPlayerOptions.locationPathName = "DetailedReportBuild/MyGame.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+
+        buildPlayerOptions.options = BuildOptions.Development | BuildOptions.DetailedBuildReport;
+        
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
